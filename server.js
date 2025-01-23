65376363cc9558327ac0fd84b02b3df65a92cc07
mohdzaker <iamzaker786@gmail.com>
2025-01-23 21:20:59 +0530

    cors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use("/api/user", userRouter);
 app.use("/api/admin", adminRouter);
