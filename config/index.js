926704344d1ba9a980dca9d75eb074a72511bab9
mohdzaker <iamzaker786@gmail.com>
2025-01-23 21:45:55 +0530

    db

--- config/index.js
+++ config/index.js
@@ -1,10 +1,18 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize( {
+if(process.env.NODE_ENV !== 'production'){
+    const sequelize = new Sequelize( {
         dialect: "sqlite",
         storage: './database.sqlite',
     }
 );
+}else{
+    const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        port: process.env.DB_PORT
+    });
+}
 
 export const connectToDb = async () => {
     try {
