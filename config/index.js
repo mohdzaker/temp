985be9bf5f06c4f0141881c6e20b9bbefb164d9c
mohdzaker <iamzaker786@gmail.com>
2025-01-23 21:52:20 +0530

    solved seq prob

--- config/index.js
+++ config/index.js
@@ -1,13 +1,15 @@
 import { Sequelize } from "sequelize";
 
+let sequelize;
+
 if(process.env.NODE_ENV !== 'production'){
-    const sequelize = new Sequelize( {
+    sequelize = new Sequelize( {
         dialect: "sqlite",
         storage: './database.sqlite',
     }
 );
 }else{
-    const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
         host: process.env.DB_HOST,
         dialect: 'mysql',
         port: process.env.DB_PORT
