25dcd56acec0133d1063a70f6ea17263f8e41600
mohdzaker <iamzaker786@gmail.com>
2025-02-04 22:18:52 +0530

    some update in signup bonus

---
+++ controllers/admin/check-diff/index.js
@@ -0,0 +1,32 @@
+import Transaction from "../../../models/Transaction.js";
+import User from "../../../models/User.js";
+
+export const getDiff = async (req, res) => {
+    try {
+        const { user_id } = req.body;
+        const userRecord = await User.findOne({ where: { id: user } });
+        const totalCreditAmount = await Transaction.sum('amount', { 
+            where: {
+              user_id: user,
+              trans_type: "credit",
+            },
+          });
+
+          const diff = totalCreditAmount - userRecord.balance;
+
+          return res.json({
+            status: "success",
+            data: {
+                actual_balance: userRecord.balance,
+                total_credit_amount: totalCreditAmount,
+                difference: diff
+            },
+        });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({
+            status: "failed",
+            message: "An error occurred while fetching the difference",
+        });
+    }
+}
