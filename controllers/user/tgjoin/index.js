59b80839c4c94d1d7cfd9488c68ba769eaee9ac3
mohdzaker <iamzaker786@gmail.com>
2025-03-11 14:54:09 +0530

    hehe

--- controllers/user/tgjoin/index.js
+++ controllers/user/tgjoin/index.js
@@ -38,7 +38,7 @@ const JoinTgAddReward = async (req, res) => {
       id: 1
     }});
 
-    const res = await axios.get(`https://api.huntcash.in/api/user/postback/?click_id=${click_id}&event=jointg&secret_key=${keys.secret_key}`);
+    const res = await axios.get(`https://api.huntcash.in/api/user/postback/?click_id=${click_id}&event=100&secret_key=${keys.secret_key}`);
     console.log(res)
     res.status(200).json({
       status: "success",
