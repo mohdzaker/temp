026f42b2b38a9390ff2a6171abcb105d79c693b3
mohdzaker <iamzaker786@gmail.com>
2025-03-11 17:32:36 +0530

    some changes

--- controllers/admin/send-notification/index.js
+++ controllers/admin/send-notification/index.js
@@ -1,20 +1,30 @@
 import { sendNotificationToUser } from "../../../utils/sendPushNotification.js";
+import User from "../../../models/User.js";
 
 const sendNotification = async (req, res) => {
     try {
-        const {user_id } = req.body;
+        const users = await User.findAll({ attributes: ['id'] });
+        const { title, description } = req.body;
+        if (!users.length) {
+            return res.status(404).json({
+                status: "failed",
+                message: "No users found"
+            });
+        }
 
-        await sendNotificationToUser("Test", "This is a test notification", user_id);
+        await Promise.all(users.map(user => 
+            sendNotificationToUser(title, description, user.id)
+        ));
 
         return res.status(200).json({
             status: "success",
-            message: "Notification sent successfully"
+            message: "Notification sent to all users successfully"
         });
     } catch (error) {
         console.log(error.message);
         return res.status(500).json({
             status: "failed",
-            message: "Failed to send notification"
+            message: "Failed to send notifications"
         });
     }
 }
