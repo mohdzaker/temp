b1dfb4c8b4c93356879f7332c407be1ef36d3d34
mohdzaker <mohammadzaker245@gmail.com>
2025-03-14 06:01:40 +0530

    some change

--- controllers/admin/callback/index.js
+++ controllers/admin/callback/index.js
@@ -7,12 +7,7 @@ const callback = async (req, res) => {
     const { data } = req.body;
     const { secret_key } = req.query;
 
-    // Validate secret key
-    const checkSecretKey = await SecretKey.findOne({
-      where: {
-        secret_key,
-      },
-    });
+    const checkSecretKey = await SecretKey.findOne({ where: { secret_key } });
 
     if (!checkSecretKey) {
       return res.status(401).json({
@@ -21,34 +16,47 @@ const callback = async (req, res) => {
       });
     }
 
-    // Validate request data
-    if (!data || !data.upi || !data.tnx_id || !data.order_id || !data.status) {
-      return res.status(400).json({ message: "Invalid request data" });
+    if (!data?.upi || !data?.tnx_id || !data?.order_id || !data?.status) {
+      return res.status(400).json({
+        status: "failed",
+        message: "Invalid request data",
+      });
     }
 
-    // Find the withdrawal record
-    const withdraw = await Withdraw.findOne({
-      where: { order_id: data.order_id },
-    });
+    const withdraw = await Withdraw.findOne({ where: { order_id: data.order_id } });
 
     if (!withdraw) {
-      return res.status(404).json({ message: "Withdrawal record not found" });
+      return res.status(404).json({
+        status: "failed",
+        message: "Withdrawal record not found",
+      });
     }
 
-    // Update the withdrawal status based on payment status
+    if (withdraw.status === data.status) {
+      return res.json({
+        status: "failed",
+        message: "Callback already processed!",
+      });
+    }
+
     await withdraw.update({
       withdraw_status: data.status === "success" ? 0 : 1,
       status: data.status,
     });
 
-    await sendNotificationToUser("Withdraw success!", "Your withdraw was successfull!", withdraw.user_id);
+    await sendNotificationToUser(
+      "Withdraw Successful!",
+      "Your withdrawal was successful!",
+      withdraw.user_id
+    );
+
     return res.json({
       status: "success",
       message: "Payment callback processed successfully",
     });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({
+    console.error("Payment Callback Error:", error);
+    return res.status(500).json({
       status: "failed",
       message: "Error processing payment callback",
     });
